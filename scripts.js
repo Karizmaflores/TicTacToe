c6e8356a00cda7dc4f3aa1f6c3660e10bcbc239d
Karizmaflores <flores_karizma@yahoo.com>
2023-02-21 20:47:40 -0600

    completed winner logic

--- scripts.js
+++ scripts.js
@@ -73,15 +73,18 @@ const horizontalWin = () => {
   if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
   || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
   )
-  
+  {
+    return true
+  }
   else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
   || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
   )
-
-  else((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  {
+    return true
+  }
+  else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
   || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
   )
-  
   {
     return true
   }
@@ -92,13 +95,17 @@ const verticalWin = () => {
   if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
   || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
   )
-   
-  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  {
+    return true
+  }
+  else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
   )
-
-  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  {
+    return true
+  }
+ else if ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+  || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
   )
 
 {
@@ -111,6 +118,13 @@ const diagonalWin = () => {
   if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
   || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
 )
+{
+  return true
+}
+else if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+)
+
 {
   return true
 }
